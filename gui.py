72b17bb7a1edd8f627328aabf0ee4a2ff340ac1b
Meguuuri <geloc611@gmail.com>
2026-03-03 14:17:47 +0800

    created class and added main.py

--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
 import tkinter as tk
+from main import oxpsConverter
+class Oxpsapp:
+    def __init__(self, window):
+        self.window = tk.Tk()
+        self.window.geometry("500x500")
+        self.window.title("OXPS to PDF Converter")
 
-window = tk.Tk()
-
-window.geometry("500x500")
-window.title("OXPS to PDF Converter")
-
-label = tk.Label(window, text ="Microsoft One Note To PDF File Converter", font=('Arial', 12))
-label.pack(padx=20, pady=20)
-
-window.mainloop()
+        self.label = tk.Label(self.window, text ="Microsoft One Note To PDF File Converter", font=('Arial', 12))
+        self.label.pack(padx=20, pady=20)
+        self.window.mainloop()
 
